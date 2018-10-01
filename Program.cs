ebb8b8923821225028f41e734fb58ca28239d34e
Niels Groeneveld <j.c.groeneveld@students.uu.nl>
2018-10-01 14:03:49 +0200

    Set up main program file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mandelbrot
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Form render = new RenderForm();
+            Application.Run(render);
+        }
+
+
+        
+    }
+}
