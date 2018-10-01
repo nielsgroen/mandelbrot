b6a12b25fd12fbcca46dcb45bb55a558857d0de6
Niels Groeneveld <j.c.groeneveld@students.uu.nl>
2018-10-01 14:32:10 +0200

    updated gui

--- RenderForm.cs
+++ RenderForm.cs
@@ -15,5 +15,35 @@ namespace mandelbrot
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
