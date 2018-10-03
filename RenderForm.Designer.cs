f533df7fccd100c364c7c9c6fbddbbd3553d89e2
Niels Groeneveld <j.c.groeneveld@students.uu.nl>
2018-10-03 12:17:51 +0200

    Corrected Mandelbrot coordinate system

--- RenderForm.Designer.cs
+++ RenderForm.Designer.cs
@@ -42,21 +42,29 @@ namespace mandelbrot
              * mandelbrotPB is the PictureBox holding the Bitmap : Image named mandelbrotBM
              */
             PictureBox mandelbrotPB = new PictureBox();
-            mandelbrotPB.Size = new Size(400, 400);
+            mandelbrotPB.Size = new Size(1000, 1000);
             mandelbrotPB.Location = new Point(10, 100);
             this.Controls.Add(mandelbrotPB);
 
-            Bitmap mandelbrotBM = new Bitmap(mandelbrotPB.Width, mandelbrotPB.Height);
+            Bitmap mandelbrotBM = new Bitmap(mandelbrotPB.Width / 2, mandelbrotPB.Height / 2);
             Graphics gr = Graphics.FromImage(mandelbrotBM);
             Mandelbrot mandelbrot = new Mandelbrot( Color.Black );
 
-            for(int i = 0; i < mandelbrotBM.Width; i++)
+            double scale = 0.01;
+            double centerX = 1;
+            double centerY = 1;
+
+
+            for (int i = 0; i < mandelbrotBM.Width; i++)
             {
-                for(int j = 0; j < mandelbrotBM.Height; j++)
+                for (int j = 0; j < mandelbrotBM.Height; j++)
                 {
-                    mandelbrotBM.SetPixel(i, j, mandelbrot.calcColor((i - mandelbrotBM.Width / 2) * 0.01, (j - mandelbrotBM.Height / 2) * 0.01, 100));
+                    double x = (i - (double) mandelbrotBM.Width / 2) * scale + centerX;
+                    double y = (j - (double) mandelbrotBM.Height / 2) * scale + centerY;
+                    mandelbrotBM.SetPixel(i, j, mandelbrot.calcColor(x, y, 100));
                 }
             }
+
             gr.DrawImage(mandelbrotBM, 0, 0, mandelbrotBM.Width, mandelbrotBM.Height);
             mandelbrotPB.Image = mandelbrotBM;
 
